d0b73572720e02778736da4875e2d4d9fd06ad75
mandeep <singh.mr.1996@gmail.com>
2021-11-05 19:11:05 +0530

    Feat:add formik and yup for validation

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,29 +1,64 @@
 import React from "react";
-import { Grid, Paper, TextField, Button, Typography, Link } from "@material-ui/core";
-import FundooHeader from "../../Components/FundooHeader"
-import "../Login/login.scss";
-const Login = () => {
-  const btnstyle = { margin: "20px 0" };
-  return (
-    <Grid>
-      <Paper className="paperstyle" elevation={10} >
-      <Grid align="center">
+import { Grid, Paper, TextField, Button, Typography } from "@material-ui/core";
+import FundooHeader from '../../Components/FundooHeader';
+import { ErrorMessage ,Formik, Field, Form} from 'formik';
+import * as yup from 'yup';
+import '../Login/login.scss';
+
+const Login =() => {
+    return (
+        <Formik
+        initialValues ={{
+            emailId:'',
+            password:''
+          }}
+          validationSchema ={ yup.object({
+            emailId: yup.string().email('Invalid Email address').required('Required'),
+            password:yup.string().required('Required')
+    
+          })}
+          onSubmit = {(values,props)=>{
+            console.log(values);
+            props.resetForm()
+         }}
+          >
+        {formik =>(
+        <Grid>
+            <Paper elevation={12} className="paperstyle" >
+                <Grid align="center">
                     <FundooHeader/>
+                <h2 className="header">Sign In</h2>
                 </Grid>
-        <TextField label="Email" placeholder="Enter email" fullWidth required />
-        <TextField label="Password" placeholder="Enter password" type="password" fullWidth required />
-        <Button type="submit" color="primary" variant="contained" style={btnstyle} fullWidth>
-          Sign in
-        </Button>
-        <Typography>
-          <Link href="#">Forgot password ?</Link>
-        </Typography>
-        <Typography>
-          Do you have an account ?<Link href="/register">Sign Up</Link>
-        </Typography>
-      </Paper>
-    </Grid>
-  );
-};
+                <Form onSubmit={formik.handleSubmit}> 
+                     <Field id="emailId"
+                      as = {TextField} 
+                      name = 'emailId'label="Email" variant="outlined"size="small"
+                      onChange = {formik.handleChange} onBlur = {formik.handleBlur}
+                      value={formik.values.emailId} helperText = {<ErrorMessage name = 'emailId'/>} fullWidth
+                      required/>
+                      
+                    <Field
+                      as = {TextField}
+                      name="password" id="password" label="Password" type="password" fullWidth variant="outlined" size="small"
+                      onChange = {formik.handleChange}
+                      helperText = {<ErrorMessage name = 'password'/>}
+                      value={formik.values.password}
+                      required  margin="normal" 
+                    />
+                      
+                  <Typography>                  
+                    <Button type = "Submit" color = "primary" variant = "contained"  className='signIn' fullWidth>
+                      Sign In
+                    </Button>
+                  </Typography>
+                <p className='register'><Button href='/register' color='primary' variant = 'text'>Create account</Button></p>          
+                </Form>
+            </Paper>
+        </Grid>
+        )}
+        </Formik>
+    )
+}
 
 export default Login;
+
