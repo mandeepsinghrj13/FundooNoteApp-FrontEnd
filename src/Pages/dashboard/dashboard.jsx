6fd5a01998cf25bffa8bc3da75854940df235aa5
mandeep <singh.mr.1996@gmail.com>
2021-11-18 16:30:10 +0530

    Refacter: sidebar of dashbar

--- src/Pages/dashboard/dashboard.jsx
+++ src/Pages/dashboard/dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import clsx from "clsx";
@@ -51,17 +52,50 @@ const useStyles = makeStyles((theme) => ({
   },
 
   drawer: {
-    top: "40px",
+    top: "65px",
     whiteSpace: "nowrap",
   },
+  drawerOpen: {
+    width: "230px",
+    borderRight: "lightgray solid 1px",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    [theme.breakpoints.down("xs")]: {
+      width: "180px",
+    },
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    borderRight: "none",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(8) + 1,
+    },
+  },
+  
+ 
+
+ 
 }));
 
 export default function Dashboard(props) {
   const history = useHistory();
   const classes = useStyles();
   const [open, setOpen] = React.useState();
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const [anchorE2, setAnchorE2] = React.useState();
-  const {search} = React.useState();
+  const [notes, setNotes] = React.useState(true);
+  const [reminders, setReminders] = React.useState(false);
+  const [editLabels, setEditLabels] = React.useState(false);
+  const [achive, setAchive] = React.useState(false);
+  const [trash, setTrash] = React.useState(false);
+  const [search, setSearch] = React.useState();
 
   const drawerOpen = () => {
     setOpen(true);
@@ -85,6 +119,10 @@ export default function Dashboard(props) {
   };
 
 
+  const drawerOpenClose = () => {
+    setOpen(!open);
+  };
+ 
   const logOut = () => {
     setTimeout(() => {
       localStorage.removeItem("token"); 
@@ -104,7 +142,9 @@ export default function Dashboard(props) {
             <div className="startOptions">
               <div className="menuButton tooltip">
                 <span class="tooltiptext">menu</span>
-                <IconButton  className={classes.appBarButton}   >
+                <IconButton  className={classes.appBarButton} 
+                 onClick={drawerOpenClose}
+                 edge="start"  >
                   <MenuIcon className={classes.iconLogo} />
                 </IconButton>
               </div>
@@ -220,7 +260,7 @@ export default function Dashboard(props) {
           <div className="drawerList">
             <List>
               <div className="drawerButton" >
-                <ListItem >
+                <ListItem  button  >
                   <ListItemIcon>
                     <svg width="30" height="30" viewBox="0 0 20 30">
                       <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6A4.997 4.997 0 0 1 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z"></path>
@@ -231,7 +271,7 @@ export default function Dashboard(props) {
               </div>
 
               <div className="drawerButton" >
-                <ListItem >
+                <ListItem button    >
                   <ListItemIcon>
                     <svg width="28" height="28" viewBox="0 0 20 30">
                       <path d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2z"></path>
@@ -242,7 +282,7 @@ export default function Dashboard(props) {
               </div>
 
               <div className="drawerButton" >
-                <ListItem >
+                <ListItem button  >
                   <ListItemIcon>
                     <svg width="28" height="28" viewBox="0 0 20 30">
                       <path d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L13.4 6.41 3 16.82V21h4.18l10.46-10.46 2.77-2.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z"></path>
@@ -253,7 +293,7 @@ export default function Dashboard(props) {
               </div>
 
               <div className="drawerButton" >
-                <ListItem  >
+                <ListItem button >
                   <ListItemIcon>
                     <svg width="28" height="28" viewBox="0 0 20 30">
                       <path d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.83 1H5.42l.82-1zM5 19V8h14v11H5zm11-5.5l-4 4-4-4 1.41-1.41L11 13.67V10h2v3.67l1.59-1.59L16 13.5z"></path>
@@ -264,7 +304,7 @@ export default function Dashboard(props) {
               </div>
 
               <div className="drawerButton" >
-                <ListItem  >
+                <ListItem  button  >
                   <ListItemIcon>
                     <svg width="28" height="28" viewBox="0 0 20 30">
                       <path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path>
