e92ec1d71df873e1d41aa15c2e7bcd4cb448724a
mandeepsinghrj13 <66105213+mandeepsinghrj13@users.noreply.github.com>
2021-12-09 08:09:28 +0530

    change ci/cd

--- src/Components/createNote/addNotes.jsx
+++ src/Components/createNote/addNotes.jsx
@@ -50,7 +50,7 @@ export default function AddNote(props) {
     else {
       Services.updateNotes(formval)
         .then((data) => {
-          toast.success("Note Updates");
+          toast.success("Note Update");
           props.getall();
         })
         .catch((err) => {
